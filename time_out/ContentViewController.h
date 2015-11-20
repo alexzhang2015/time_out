b16339d2201a6109de099bbbe6256f06548c2c10
alex <chenhua.zhang@ele.me>
2015-11-20 10:34:27 +0800

    add the popover winodw for memubar icon

---
+++ time_out/ContentViewController.h
@@ -0,0 +1,14 @@
+//
+//  ContentViewController.h
+//  Copyright 2011-2014 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ContentViewController : NSViewController {
+@private
+    
+}
+
+@end
---
+++ time_out/ContentViewController.m
@@ -0,0 +1,22 @@
+//
+//  ContentViewController.m
+//  Copyright 2011-2014 Indragie Karunaratne. All rights reserved.
+//
+
+#import "ContentViewController.h"
+
+
+@implementation ContentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+
+@end
